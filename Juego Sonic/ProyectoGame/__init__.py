87aac6aa601b261136c8c4c961e891a4f4eb4161
Erick Bolaños <38430620+EddRick96@users.noreply.github.com>
2019-01-08 21:01:19 -0500

    Add files via upload

--- Juego Sonic/ProyectoGame/__init__.py
+++ Juego Sonic/ProyectoGame/__init__.py
@@ -5,7 +5,7 @@
 import sys, pygame
 from pygame.locals import *
 from time import clock
-#from pygame.constants import K_UP 
+from pygame.constants import K_UP 
 
 #Tamaño de la pantalla
 WIDTH = 1200
@@ -17,12 +17,104 @@ SposY = 364
 cont=6
 direc=True
 i=0
-sonic_camina={}
-Rsonic_camina={}
 
-parabola={}
+bajada = False
+
 salto = False
 salto_Par = False
+
+#===============================================
+
+def Initialize():
+   
+    global screen, clock,sonic_camina,Rsonic_camina
+   
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Sonic Game")
+    clock = pygame.time.Clock()
+   
+    sonic_camina={}
+    Rsonic_camina={}
+   
+    sonic_camina[0]=(1,0,29,41)
+    sonic_camina[1]=(29,0,54,41)
+    sonic_camina[2]=(56,0,80,41)
+    sonic_camina[3]=(80,0,116,41)
+    sonic_camina[4]=(116,0,153,41)
+    sonic_camina[5]=(153,0,32,41)
+    
+    Rsonic_camina[0]=(482,1,511,41)
+    Rsonic_camina[1]=(456,1,482,41)
+    Rsonic_camina[2]=(431,1,456,41)
+    Rsonic_camina[3]=(395,0,431,41)
+    Rsonic_camina[4]=(358,0,395,41)
+    Rsonic_camina[5]=(324,0,358,41)
+   
+    return
+#===============================================
+def LoadContent():
+   
+    global fondo, sonic,sonic_inv  
+   
+    fondo = imagen("Imagenes/stage1_vacio.png").convert()
+    sonic = imagen("Imagenes/Sonic1.png", True)
+    sonic_inv = pygame.transform.flip(sonic, True, False)
+   
+    return
+#===============================================
+def Updates():
+   
+    teclado()    
+    #Escenario
+    sprite_S()  
+    #Enemigo()
+    #Coliciones()
+   
+    return
+#===============================================
+def Draw():
+   
+    global salto,salto_Par, salto,bajada_Par,bajada
+   
+   
+    screen.blit(fondo, (0, 0))
+       
+       
+    global SposX, SposY
+       
+    if direc==True and salto==False :
+        screen.blit(sonic, ( SposX, SposY),(sonic_camina[i]))
+   
+    if direc==False and salto==False :
+        screen.blit(sonic_inv, ( SposX, SposY),(Rsonic_camina[i]))
+       
+       
+#salto normal y Parabolico
+    if salto==True:            
+            if direc==True:
+                screen.blit(sonic, ( SposX, SposY),(sonic_camina[4]))
+            if direc==False:
+                screen.blit(sonic_inv, ( SposX, SposY),(Rsonic_camina[4]))  
+           
+            if bajada==False:
+                SposY-=4              
+               
+            if bajada==True:
+                SposY+=4              
+           
+            if SposY<=232:
+                bajada=True
+           
+            if SposY>=364:
+                bajada=False
+                salto=False
+#============================================== 
+             
+   
+    pygame.display.flip()
+   
+    return
 #=================IMAGEN========================
 def imagen(filename, transparent=False):
     try:
@@ -39,51 +131,37 @@ def imagen(filename, transparent=False):
 def teclado():
     
     global SposX
-    global cont, direc, salto, salto_Par
+    global cont, direc, salto
     
-    teclado=pygame.key.get_pressed()
-    
+    teclado = pygame.key.get_pressed()
    
-    if teclado[K_UP] and teclado[K_RIGHT] and salto_Par==False:
-        salto_Par=True
-    elif teclado[K_UP] and teclado[K_LEFT] and salto_Par==False:
-        salto_Par=True
-         
-    elif teclado[K_RIGHT]and salto==False and salto_Par==False:
+    if teclado[K_UP]:
+        salto=True
+       
+    if teclado[K_RIGHT]:
         SposX+=2
         cont+=1
         direc=True
-    elif teclado[K_LEFT]and salto==False and salto_Par==False:
+    elif teclado[K_LEFT]:
         SposX-=2
         cont+=1
-        direc=False
-    elif teclado[K_UP] and salto==False and salto_Par==False:
-        salto=True          
+        direc=False            
     else :
         cont=6
+         
    
-       
-       
+    # Cerrar la ventana
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+   
+         
     return
 #===================================================
-#=============SPRITE=============================
-def sprite():
+#=============SPRITE================================
+def sprite_S():
+    
     global cont
-    
-    sonic_camina[0]=(1,0,29,41)
-    sonic_camina[1]=(29,0,54,41)
-    sonic_camina[2]=(56,0,80,41)
-    sonic_camina[3]=(80,0,116,41)
-    sonic_camina[4]=(116,0,153,41)
-    sonic_camina[5]=(153,0,32,41)
-    
-    Rsonic_camina[0]=(482,1,511,41)
-    Rsonic_camina[1]=(456,1,482,41)
-    Rsonic_camina[2]=(431,1,456,41)
-    Rsonic_camina[3]=(395,0,431,41)
-    Rsonic_camina[4]=(358,0,395,41)
-    Rsonic_camina[5]=(324,0,358,41)
-
     p=6
    
     global i
@@ -111,111 +189,28 @@ def sprite():
 
 #================================================ 
 def main():
-    
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Sonic Game")
-    
-    fondo=imagen("Imagenes/stage1_vacio.png").convert()
-    
-    
-    sonic=imagen("Imagenes/Sonic1.png", True)
-    sonic_inv=pygame.transform.flip(sonic, True, False)
-    
-    reloj=pygame.time.Clock()
-    
-
-    
-    global salto_Par
-    bajada = False
-    bajada_Par = False             
-    #Bucle principal del juego 
-    while True:
-          
-        time = reloj.tick(140)     
-        sprite()
-        teclado()
-       
-        
-        screen.blit(fondo,(0,0))
-        
-        global SposX, SposY, salto
-        
-        if direc==True:
-            screen.blit(sonic, ( SposX, SposY),(sonic_camina[i]))
    
-        if direc==False:
-            screen.blit(sonic_inv, ( SposX, SposY),(Rsonic_camina[i]))
-           
-        #Salto Normal
-        if salto==True:            
-           
-            if direc==True:
-                screen.blit(sonic, ( SposX, SposY),(sonic_camina[4]))
-            if direc==False:
-                screen.blit(sonic_inv, ( SposX, SposY),(Rsonic_camina[4]))  
-           
-            if bajada==False:
-                SposY-=4              
-               
-            if bajada==True:
-                SposY+=4              
-           
-            if SposY==232:
-                bajada=True
-           
-            if SposY==364:
-                bajada=False
-                salto=False
-        #======================================  
+    Initialize()
+   
+    LoadContent()
+   
        
-        #SALTO PARABOLICO
-        if salto_Par==True and direc==True:            
-           
-            screen.blit(sonic, ( SposX, SposY),(sonic_camina[4]))
-           
-            if bajada_Par==False:
-                SposY-=3
-                SposX+=2
-               
-            if bajada_Par==True:
-                SposY+=3
-                SposX+=2
-           
-            if SposY==292:
-                bajada_Par=True
-           
-            if SposY==364:
-                bajada_Par=False
-                salto_Par=False
-        elif salto_Par==True and direc==False:            
-           
-            screen.blit(sonic_inv, ( SposX, SposY),(Rsonic_camina[4]))
-           
-            if bajada_Par==False:
-                SposY-=3
-                SposX-=2
-               
-            if bajada_Par==True:
-                SposY+=3
-                SposX-=2
-           
-            if SposY==292:
-                bajada_Par=True
-           
-            if SposY==364:
-                bajada_Par=False
-                salto_Par=False  
-        
-        pygame.display.flip()
-        
-        #Cerrar la ventana
-        
-        for eventos in pygame.event.get():
-            if eventos.type == QUIT:
-                sys.exit(0)
-        
-    return 0
+    while True:
+       
+        time = clock.tick(60)
+       
+        Updates()
+       
+        Draw()
+       
+       
+       
+        #if gameOver==True
+            #UnLoadContent()
+         
+     
+   
+    return
  
 if __name__ == '__main__':
     
